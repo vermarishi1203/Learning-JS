2e3f65b774f7f3d61195c1d062c0c292fae0893c
vermarishi1203 <vermarishi1203@gmail.com>
2024-07-06 12:49:44 +0530

    added some explanatory comments to self

--- slice-method.js
+++ slice-method.js
@@ -5,16 +5,16 @@ const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // getting the elements of an array from index n to index m
 
-console.log( numArr.slice(2,5)); // starting index element is included but ending index element is not 
+console.log( numArr.slice(2,5)); //n=2 and m=5 in this example; starting index element is included but ending index element is not 
 
 // getting the elements from index n element to the last element 
 
-console.log( numArr.slice(6)); //ending index parameter is not passed so it takes array.length as the value(not the last element index as that is not included)
+console.log( numArr.slice(6)); //n= 6 in this example ; ending index parameter is not passed so it takes array.length as the value(not the last element index as that is not included)
 
 // getting the first n elements
 
-console.log( numArr.slice(0, 4));
+console.log( numArr.slice(0, 4)); //n = 4 in this example
 
 //getting the last n elements
 
-console.log( numArr.slice(-3));
+console.log( numArr.slice(-3)); //n = 3 in this example

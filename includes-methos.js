57d744de7422f9b497b707a45b64cebd9a05be20
vermarishi1203 <vermarishi1203@gmail.com>
2024-07-07 20:41:30 +0530

    learned to use includes array method

---
+++ includes-methos.js
@@ -0,0 +1,8 @@
+
+// trying the includes method
+
+// checking if we have a certain person present in the array
+
+const peopleArr = ["Rishi", "Aniket", "Dev", "Shubham"];
+
+console.log(peopleArr.includes("Aniket"));

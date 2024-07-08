2d2d7f56c3c6b054a20a85796a24baeaf2f3d035
vermarishi1203 <vermarishi1203@gmail.com>
2024-07-08 16:02:17 +0530

    practiced the join array method

---
+++ join-method.js
@@ -0,0 +1,6 @@
+
+// I have used join() method before , but lets brush up our knowledge
+
+const peopleArr = ["Rishi", "Aniket", "Dev", "Shubham"];
+
+console.log(peopleArr.join(", ")+" are the most awesome people on the planet!!!");

9b1d20c6d223f91307231f59b548d17f34f3d102
vermarishi1203 <vermarishi1203@gmail.com>
2024-07-08 16:29:25 +0530

    practiced the pop array method

---
+++ pop-method.js
@@ -0,0 +1,8 @@
+
+// same like push(), i've used pop() multiple times but let's practice
+
+// poping an element from the array
+
+const numArr = [1, 2, 3, 4];
+
+console.log( numArr.pop(), numArr);

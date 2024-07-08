2fa0603380d88bfe8d61161f1e9fdc8bf383996b
vermarishi1203 <vermarishi1203@gmail.com>
2024-07-08 16:38:32 +0530

    learned the unshift array method

---
+++ unshift-method.js
@@ -0,0 +1,13 @@
+
+// trying the unshift() method
+
+// adding an element in the beginning of an array
+
+const numArr = [4, 5, 6];
+
+console.log(numArr.unshift(3), numArr);
+
+// adding multiple elements in the beginning of the array
+
+console.log(numArr.unshift(1, 2), numArr); // be mindful of the order of parameters passed
+

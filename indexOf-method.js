59a8c1c4e1635a9f92d0038e043db3bcd47d38ee
vermarishi1203 <vermarishi1203@gmail.com>
2024-07-08 16:51:24 +0530

    learned indexOf array method

---
+++ indexOf-method.js
@@ -0,0 +1,11 @@
+
+// trying the indexOf() method
+
+// finding the index of an element
+
+const peopleArr= ["Rishi", "Dev", "Aniket","Rishi",  "Shubham", "Rishi"];
+
+console.log(peopleArr.indexOf("Rishi")); // only returns the index of the first appearance of the element
+
+console.log(peopleArr.indexOf("Atul")); // return -1 if element isn't in the array
+

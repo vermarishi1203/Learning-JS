70c591af38966f73eeb6b5bf3180568f5fd6ae1b
vermarishi1203 <vermarishi1203@gmail.com>
2024-07-08 16:23:45 +0530

    practised push array method

---
+++ push-method.js
@@ -0,0 +1,16 @@
+
+// I am pretty much familiar with push() method but practice never hurts
+
+// pushing elements into the array
+
+const numArr = [1, 2, 3];
+
+numArr.push(4);
+
+console.log(numArr);
+
+// pushing multiple elements at once
+
+numArr.push(5, 6, 7, 8);
+
+console.log(numArr);

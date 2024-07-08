fa1298197863313d84f2b3ec467589173f419628
vermarishi1203 <vermarishi1203@gmail.com>
2024-07-08 16:58:22 +0530

    learned lastIndexOf array method

---
+++ lastIndexOf-method.js
@@ -0,0 +1,9 @@
+
+// trying the lastIndexOf() method 
+
+const peopleArr= ["Rishi", "Dev", "Aniket", "Rishi", "Shubham", "Rishi"];
+
+console.log(peopleArr.lastIndexOf("Rishi")); // only returns the index of the last apperance of the element
+
+console.log(peopleArr.lastIndexOf("Atul")); // returns -1 if the element doesn't exists in the array
+

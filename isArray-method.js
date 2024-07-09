2e58d53246c7338b8ab22f5944a485826484bbb9
vermarishi1203 <vermarishi1203@gmail.com>
2024-07-09 12:59:12 +0530

    learned to use the array isArray method

---
+++ isArray-method.js
@@ -0,0 +1,25 @@
+
+// trying the isArray() method 
+
+// checking if certain variable/values are arrays or not
+
+const numArr = [1, 2, 3, 4, 5, 6];
+
+const strOfNum = "123456";
+
+const arrOfStr = ["A", "B", "C"];
+
+const someNum = 259;
+
+const someObj = {name: "Rishi", age: 58};
+
+console.log( Array.isArray( numArr ));
+
+console.log( Array.isArray( strOfNum ));
+
+console.log( Array.isArray( arrOfStr ));
+
+console.log( Array.isArray( someNum ));
+
+console.log( Array.isArray( someObj ));
+

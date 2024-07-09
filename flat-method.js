01558cf3edb49269f5e03fba0af987941f1e91d8
vermarishi1203 <vermarishi1203@gmail.com>
2024-07-09 13:14:08 +0530

    learned to use the array flat method

---
+++ flat-method.js
@@ -0,0 +1,7 @@
+
+// trying the flat() method 
+
+const someNestedArray = [1, [2, [3, [4, [5]]]]];
+
+console.log(someNestedArray.flat(Infinity)); // parameter is depth upto which the flat method will go , default depth is 1
+

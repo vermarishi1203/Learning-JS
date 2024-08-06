254c8c89e8ec33ca38c68323c7e7ee8860dcaca0
vermarishi1203 <vermarishi1203@gmail.com>
2024-08-06 22:47:48 +0530

    added prompt

---
+++ Recursion/app.mjs
@@ -0,0 +1,14 @@
+// Question: 
+
+
+"use strict";
+
+import promptSync from "prompt-sync";
+
+const prompt = promptSync({sigint: true});
+
+const inputNum = prompt("enter the decimal num: ");
+
+
+
+

5668cdbbc947b754f5fe5bb738a38a745b71587f
vermarishi1203 <vermarishi1203@gmail.com>
2024-08-07 14:44:51 +0530

    Functionality added: check input

--- Recursion/app.mjs
+++ Recursion/app.mjs
@@ -7,19 +7,33 @@ import promptSync from "prompt-sync";
 
 const prompt = promptSync({sigint: true});
 
-const items = prompt(`Enter the number of items: `);
+checkInput();
 
-const parts = prompt(`Enter the number of parts: `);
+function checkInput () {
 
-console.log(`no of ways: ${findWays(items, parts)}`);
+    const n = prompt(`Enter the number of items: `);
+
+    const m = prompt(`Enter the number of parts: `);
+
+    if ( m < 0 || m === "" || n === "" || isNaN(m) || isNaN(n)) {
+     
+        console.log("Invalid Input! \nPlease make sure that the values are non-negative integers");
+
+        return checkInput();
+        
+    };
+
+    console.log(`no of ways: ${findWays(n, m)}`);
+
+};
 
 function findWays ( n, m ) {
 
-    if ( m === 0 ) return 0;
+    if ( m == 0 ) return 0;
 
-    if ( n === 0 ) return 1;
+    if ( n == 0 || n < 0) return 1;
 
-    return findWays( n-m, m ) + findWays( n , m-1 );
+    return findWays( n-m , m ) + findWays( n , m-1 );
 
 };
 

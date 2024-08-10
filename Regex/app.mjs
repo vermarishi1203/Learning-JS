a311c1e9e13631615a0a90c48e92b9031d7996c9
vermarishi1203 <vermarishi1203@gmail.com>
2024-08-10 20:59:17 +0530

    learning update

--- Regex/app.mjs
+++ Regex/app.mjs
@@ -1,3 +1,3 @@
 // lets see 
 
-console.log("hello world, teach me regex !")
+console.log("The only thing i know so far about regex is that it is a term that stand for Regular Expression. I am going to hit up Eloquent Javascript to get some info. Hope I will learn it soon.")

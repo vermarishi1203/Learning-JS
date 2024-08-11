0078c115670bdc5f1b519b654e09082b8e41f229
vermarishi1203 <vermarishi1203@gmail.com>
2024-08-11 20:53:19 +0530

    learnt how to create regex

--- Regex/app.mjs
+++ Regex/app.mjs
@@ -1,3 +1,16 @@
 // lets see 
 
-console.log("The only thing i know so far about regex is that it is a term that stand for Regular Expression. I am going to hit up Eloquent Javascript to get some info. Hope I will learn it soon.")
+// Ways to create regex
+
+// using RegExp constructor
+
+const regex1 = new RegExp("abc");
+
+console.log(regex1);
+
+// directly writing a literal value
+
+const regex2 = /xyz/ ;
+
+console.log(regex2);
+

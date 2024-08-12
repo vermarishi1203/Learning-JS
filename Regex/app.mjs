97e2f22d633a3abe6bfd39b4ed822bf55f6443ba
vermarishi1203 <vermarishi1203@gmail.com>
2024-08-12 17:34:31 +0530

    how to test regex

--- Regex/app.mjs
+++ Regex/app.mjs
@@ -10,7 +10,17 @@ console.log(regex1);
 
 // directly writing a literal value
 
-const regex2 = /xyz/ ;
+const regex2 = /abc/;
 
 console.log(regex2);
+
+// testing using a regex
+
+console.log(regex1.test("abcdefg"));
+
+console.log(regex1.test("abdfc"));
+
+console.log(regex1.test("deabcf"));
+
+
 

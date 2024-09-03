42845ed5217cf1e426c90a982800d81f1e1241c9
vermarishi1203 <vermarishi1203@gmail.com>
2024-09-03 18:57:23 +0530

    added flags file

---
+++ Regex/flags.mjs
@@ -0,0 +1,6 @@
+// Flags 
+
+console.log(/the/g.exec("The cat is out of the bag because the kid turned the bag upside down on the table."));
+
+console.log(/the/i.exec("The cat is out of the bag because the kid turned the bag upside down on the table."));
+

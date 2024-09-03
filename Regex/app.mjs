f31c809b69d791e9fcd79f24fcc33ff9fc6f415b
vermarishi1203 <vermarishi1203@gmail.com>
2024-09-03 19:01:33 +0530

    removed flags from main file

--- Regex/app.mjs
+++ Regex/app.mjs
@@ -203,9 +203,3 @@
 
 // console.log(/\d+ (apple|orange|banana)s?/.test("6 birds"));
 
-// Flags 
-
-console.log(/the/g.exec("The cat is out of the bag because the kid turned the bag upside down on the table."));
-
-console.log(/the/i.exec("The cat is out of the bag because the kid turned the bag upside down on the table."));
-

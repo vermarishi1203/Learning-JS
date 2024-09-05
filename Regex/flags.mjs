e412847aeb2e5282825e6bb5df6212865219cbd1
vermarishi1203 <vermarishi1203@gmail.com>
2024-09-05 20:02:16 +0530

    added multiline flag

--- Regex/flags.mjs
+++ Regex/flags.mjs
@@ -4,3 +4,4 @@ console.log(/the/g.exec("The cat is out of the bag because the kid turned the ba
 
 console.log(/the/i.exec("The cat is out of the bag because the kid turned the bag upside down on the table.")); // case-insensitive flag
 
+console.log(/(T|t)he/m.exec("The cat is out of the bag. the kid turned the bag upside down on the table.")) //mutliline flag
